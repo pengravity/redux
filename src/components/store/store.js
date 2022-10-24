2008e36f08a68993441f9516c203ea4dcaa00d59
Andy <alipinski@mail.com>
2022-10-24 13:21:31 +0200

    create reducer and pass it to the store

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -1,8 +1,26 @@
 import { createStore } from 'redux';
 
 // Reducer
+const countReducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'SUBTRACT': {
+      return state - 1;
+    }
+    case 'RESET': {
+      return (state = 0);
+    }
+
+    case 'ADD': {
+      return state + 1;
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
 
 // create a store
-const store = createStore();
+const store = createStore(countReducer);
 
 export default store;
